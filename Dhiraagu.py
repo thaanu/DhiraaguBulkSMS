97ffc41e6445e0cef35ec145668ad8375cb117d6
Ahmed Shan <ahmedshan16@gmail.com>
2020-07-21 19:18:35 +0500

    Init

--- Dhiraagu.py
+++ Dhiraagu.py
@@ -9,29 +9,28 @@ class DhiraaguBulkSMS:
     # Send message
     def send_message(self, mobile_number, msg):
         if not self.username or not self.password:
-            print("Username and password not set")
-            quit()
+            return "Username and password not set"
 
-        self.mobile_number = mobile_number
+        self.mobile_number = '960' + mobile_number
 
         # Validate mobile number
         if self.validate_number() == False:
-            print("Invalid mobile number")
-            quit()
+            return "Invalid mobile number"
 
         enc_msg = urllib.parse.quote(msg)
-        requests.get('https://bulkmessage.dhiraagu.com.mv/jsp/receiveSMS.jsp?userid='+self.username+'&password='+self.password+'&to='+self.mobile_number+'&text='+enc_msg)
+        response = requests.get('https://bulkmessage.dhiraagu.com.mv/jsp/receiveSMS.jsp?userid=' + self.username + '&password=' + self.password + '&to=' + self.mobile_number + '&text=' + enc_msg)
+
+        return response.content.decode('utf-8')
 
     # Validate mobile number
     def validate_number(self):
+
         # Check if mobile number has 10 digits
         if len(self.mobile_number) != 10:
             return False
-        # Check if mobile number prefix starts from 960
-        if self.mobile_number[0:3] != '960':
-            return False
+
         # Check if mobile number starts with 7 or 9
-        if self.mobile_number[4] != '7' or self.mobile_number[4] != '9':
+        if self.mobile_number[4] != '7' and self.mobile_number[4] != '9':
             return False
 
         # If all good, return true
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # DhiraaguBulkSMS
 
 WebSMS server for Dhiraagu API
+
+Dhiraagu SMS gateway works locally, therefore mobile numbers should start from 7 or 9
+Prefix 960 will be preset
